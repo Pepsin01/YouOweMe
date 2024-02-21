0600939cad22f317f3e713fca1cdec523e841df8
Zechiax <106590288+Zechiax@users.noreply.github.com>
2024-02-21 18:22:39 +0100

    update home page list style

--- app/src/main/java/com/youoweme/views/HomeScreenView.kt
+++ app/src/main/java/com/youoweme/views/HomeScreenView.kt
@@ -1,24 +1,23 @@
 package com.youoweme.views
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -28,8 +27,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.youoweme.model.event.Event
@@ -63,39 +62,30 @@ fun HomeScreenView(onNavigateToEvent: (eventId: Long) -> Unit, homeScreenViewMod
             }
         }
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.inverseOnSurface),
         ) {
-            Row(modifier = Modifier
-                .fillMaxHeight(0.9F)
-                .padding(20.dp)
-                .border(1.dp, Color.Black)
-            ) {
-                LazyColumn(
+            items(uiState.events) { event ->
+                Surface(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(20.dp)) {
-                    items(uiState.events) { event ->
-                        Row(
-                            modifier = Modifier
-                                .height(50.dp)
-                                .fillMaxWidth(),
-                        ) {
-                            Button(onClick = {
-                                onNavigateToEvent(event.id)
-                            },
-                                modifier = Modifier
-                                    .fillParentMaxWidth(),
-                                shape = RoundedCornerShape(15)
-                            ) {
-                                Row {
-                                    Column {
-                                        Text(text = event.title)
-                                    }
-                                }
-                            }
+                        .fillMaxSize()
+                        .padding(5.dp)
+                        .clickable(onClick = { onNavigateToEvent(event.id) }),
+                    shape = RoundedCornerShape(16.dp),
+                    shadowElevation = 4.dp,
+                    color = MaterialTheme.colorScheme.surface
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column {
+                            Text(text = event.title)
                         }
                     }
                 }
