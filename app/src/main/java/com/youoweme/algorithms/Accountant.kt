f57aef2ca972cbae98e8d04bc38991f841fa7d1d
tomas tillmann <tomas.tilllmann@gmail.com>
2024-02-27 18:06:52 +0100

    accountant computes in double + added double test

--- app/src/main/java/com/youoweme/algorithms/Accountant.kt
+++ app/src/main/java/com/youoweme/algorithms/Accountant.kt
@@ -5,16 +5,17 @@ import com.youoweme.model.person.Person
 import com.youoweme.model.transaction.Transaction
 
 //TODO: nechci tu posilat eventId, k cemu?
-class Accountant(private val eventId: Long) {
+class Accountant(private val eventId: Long, private val epsilon: Double = 0.01) {
     fun getDebts(persons: List<Person>, transactions: List<Transaction>): List<Debt> {
         val debts = mutableListOf<Debt>()
 
-        val balances = persons.toMutableList().map { person -> Person(person.eventId, person.name, person.balance, person.id) }.sortedByDescending { person -> person.balance }
+        updateBalances(persons, transactions)
+        val balances = persons.toList().sortedByDescending { person -> person.balance }
 
         var i : Int = 0
         var j : Int = balances.size - 1
 
-        while (i <= j) {
+        while (i < j) {
             debts.add(transaction(balances[i], balances[j], eventId))
 
             if (balances[i].balance == 0.0) {
@@ -30,7 +31,7 @@ class Accountant(private val eventId: Long) {
     }
 
     private fun transaction(person1: Person, person2: Person, eventId: Long /* ugly */): Debt {
-        if (person1.balance + person2.balance < 0) {
+        if (person1.balance + person2.balance < 0.0) {
             val debt: Debt = Debt(eventId /* ugly :( */, person1.balance, person2.id, person1.id)
 
             person2.balance += person1.balance
@@ -45,5 +46,20 @@ class Accountant(private val eventId: Long) {
         person2.balance = 0.0
 
         return debt
+    }
+
+    private fun updateBalances(persons: List<Person>, transactions: List<Transaction>): Unit
+    {
+        val personToBalance : MutableMap<Long, Double> = persons.associateBy({ it.id }, { 0.0 }).toMutableMap()
+
+        for (transaction in transactions) {
+            personToBalance[transaction.payerId] = personToBalance[transaction.payerId]!! + transaction.amount
+            personToBalance[transaction.payeeId] = personToBalance[transaction.payeeId]!! - transaction.amount
+        }
+
+        for (person in persons)
+        {
+            person.balance = personToBalance[person.id]!!
+        }
     }
 }
--- app/src/test/java/com/youoweme/AccountantAlgorithmTest.kt
+++ app/src/test/java/com/youoweme/AccountantAlgorithmTest.kt
@@ -4,42 +4,86 @@ import com.youoweme.algorithms.Accountant
 import com.youoweme.model.debt.Debt
 import com.youoweme.model.person.Person
 import com.youoweme.model.transaction.Transaction
-import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
 
 class AccountantAlgorithmTest {
     @Test
-    fun getDebtsTest_1() {
+    fun getDebtsTest_Ints() {
         // arrange
         val accountant: Accountant = Accountant(0)
         val persons = listOf<Person>(
-            Person(0, "C", 3.0, 3),
-            Person(0, "D", -3.0, 4),
-            Person(0, "E", -4.0, 5),
-            Person(0, "G", -6.0, 7),
-            Person(0, "F", -5.0, 6),
-            Person(0, "A", 8.0, 1),
-            Person(0, "B", 7.0, 2),
+            Person(0, "A", 0.0, 1),
+            Person(0, "B", 0.0, 2),
+            Person(0, "C", 0.0, 3),
+            Person(0, "D", 0.0, 4),
+            Person(0, "E", 0.0, 5),
+            Person(0, "F", 0.0, 6),
+            Person(0, "G", 0.0, 7),
 
         )
-        val transactions = listOf<Transaction>()
+        val transactions = mutableListOf<Transaction>();
+        transactions.add(Transaction(0, 4.0, 3, 1, ""))
+        transactions.add(Transaction(0, 2.0, 4, 3, ""))
+        transactions.add(Transaction(0, 1.0, 1, 4, ""))
+        transactions.add(Transaction(0, 4.0, 2, 1, ""))
+        transactions.add(Transaction(0, 1.0, 1, 5, ""))
+        transactions.add(Transaction(0, 3.0, 7, 2, ""))
+        transactions.add(Transaction(0, 6.0, 5, 7, ""))
+        transactions.add(Transaction(0, 1.0, 7, 6, ""))
 
         // act
         val debts = accountant.getDebts(persons, transactions)
 
         // assert
         assertEquals(5, debts.size)
-        AssertEqual(Debt(0, 6.0, 7, 1), debts[0])
-        AssertEqual(Debt(0, 2.0, 6, 1), debts[1])
-        AssertEqual(Debt(0, 3.0, 6, 2), debts[2])
-        AssertEqual(Debt(0, 4.0, 5, 2), debts[3])
-        AssertEqual(Debt(0, 3.0, 4, 3), debts[4])
+        assertEquals(Debt(0, 5.0, 1, 5), debts[0])
+        assertEquals(Debt(0, 1.0, 1, 3), debts[1])
+        assertEquals(Debt(0, 1.0, 7, 3), debts[2])
+        assertEquals(Debt(0, 1.0, 7, 2), debts[3])
+        assertEquals(Debt(0, 1.0, 6, 4), debts[4])
     }
 
-    private fun AssertEqual(expected: Debt, actual: Debt) {
-        assertEquals(expected.amount, actual.amount, 0.01)
+    @Test
+    fun getDebtsTest_Doubles() {
+        // arrange
+        val accountant: Accountant = Accountant(0)
+        val persons = listOf<Person>(
+            Person(0, "A", 0.0, 1),
+            Person(0, "B", 0.0, 2),
+            Person(0, "C", 0.0, 3),
+            Person(0, "D", 0.0, 4),
+            Person(0, "E", 0.0, 5),
+            Person(0, "F", 0.0, 6),
+            Person(0, "G", 0.0, 7),
+
+            )
+        val transactions = mutableListOf<Transaction>();
+        transactions.add(Transaction(0, 4.21111, 3, 1, ""))
+        transactions.add(Transaction(0, 2.100000001, 4, 3, ""))
+        transactions.add(Transaction(0, 1.25, 1, 4, ""))
+        transactions.add(Transaction(0, 4.99, 2, 1, ""))
+        transactions.add(Transaction(0, 1.99999999, 1, 5, ""))
+        transactions.add(Transaction(0, 3.49, 7, 2, ""))
+        transactions.add(Transaction(0, 6.89, 5, 7, ""))
+        transactions.add(Transaction(0, 1.79, 7, 6, ""))
+
+        // act
+        val debts = accountant.getDebts(persons, transactions)
+
+        // assert
+        assertEquals(6, debts.size)
+        assertEquals(Debt(0, 4.89, 1, 5), debts[0])
+        assertEquals(Debt(0, 1.06, 1, 3), debts[1])
+        assertEquals(Debt(0, 1.049, 6, 3), debts[2])
+        assertEquals(Debt(0, 0.74, 6, 2), debts[3])
+        assertEquals(Debt(0, 0.759, 7, 2), debts[4])
+        assertEquals(Debt(0, 0.85, 7, 4), debts[5])
+    }
+
+    private fun assertEquals(expected: Debt, actual: Debt) {
+        assertEquals(expected.amount, actual.amount, 0.1)
         assertEquals(expected.creditorId, actual.creditorId)
         assertEquals(expected.debtorId, actual.debtorId)
     }
